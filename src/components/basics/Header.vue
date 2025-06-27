f2f8df3c9f58cae83bb2421222d381a10548d89c
Mattius DT <jeronimate@gmail.com>
2025-06-27 19:29:16 +0200

    Header My Acount DropLink

--- src/components/basics/Header.vue
+++ src/components/basics/Header.vue
@@ -2,38 +2,32 @@
   <div id="header" class="container-lg">
     <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
       <div
-        class="
-          container-lg
-          d-flex
-          justify-content-between
-          px-sm-3 px-md-5
-          py-3
-          border-bottom border-secondary
-        "
+        class="container-lg d-flex justify-content-between px-sm-3 px-md-5 py-3 border-bottom border-secondary"
       >
         <router-link class="navbar-brand" to="/">
-        <img id="logo" src="../../assets/img/logo.webp"  alt="">
+          <img id="logo" src="../../assets/img/logo.webp" alt="" />
           <img
-              class="img-fluid"
-              src="../../assets/img/xayona.webp"
-              alt=""
-              width="150px" height="100px"
+            class="img-fluid"
+            src="../../assets/img/xayona.webp"
+            alt=""
+            width="150px"
+            height="100px"
           />
         </router-link>
         <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div
-            class="collapse navbar-collapse col-auto justify-content-end"
-            id="navbarNav"
+          class="collapse navbar-collapse col-auto justify-content-end"
+          id="navbarNav"
         >
           <ul class="navbar-nav">
             <li class="nav-item">
@@ -46,22 +40,47 @@
               >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link text-secondary mx-2" to="#">About</router-link>
+              <router-link class="nav-link text-secondary mx-2" to="#"
+                >About</router-link
+              >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link text-secondary mx-2" to="/shop">Shop</router-link>
+              <router-link class="nav-link text-secondary mx-2" to="/shop"
+                >Shop</router-link
+              >
             </li>
             <li class="nav-item">
-              <router-link class="nav-link text-secondary mx-2" to="#">Services</router-link>
+              <router-link class="nav-link text-secondary mx-2" to="#"
+                >Services</router-link
+              >
             </li>
-            <li class="nav-item">
-              <button>
-                <router-link class="nav-link text-secondary mx-2" to="/login">Login</router-link>
-              </button>
-              <button>
-                <router-link class="nav-link text-secondary mx-2" to="/signup">SignUp</router-link>
-              </button>
-              
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img src="../../assets/img/logo.webp" alt="" width="30px"> Mi cuenta
+              </a>
+              <ul class="dropdown-menu dropdown-menu-end">
+                <li>
+                  <router-link class="dropdown-item" to="/user/profile">
+                    <i class="fas fa-id-badge me-2"></i> Mi Perfil
+                  </router-link>
+                </li>
+                <li>
+                  <router-link class="dropdown-item" to="/user/orders">
+                    <i class="fas fa-box me-2"></i> Mis Pedidos
+                  </router-link>
+                </li>
+                <li>
+                  <router-link class="dropdown-item" to="/user/wishlist">
+                    <i class="fas fa-heart me-2"></i> Lista de deseos
+                  </router-link>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
@@ -81,12 +100,11 @@ div.collapse {
   float: right;
 }
 #navbar {
-    background: url('../../assets/img/bg-header.webp') no-repeat center;
-    background-size: cover;
+  background: url("../../assets/img/bg-header.webp") no-repeat center;
+  background-size: cover;
 }
 
-
-img#logo{
+img#logo {
   width: 10vh;
 }
 </style>
